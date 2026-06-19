2745ae6c8202649f3c23c4f8f1d4d5af9698f01c
Santosh Kumar Dash <dashsantosh1333@gmail.com>
2026-06-19 09:40:54 +0530

    add mongoose and database connection setup

--- src/index.js
+++ src/index.js
@@ -3,9 +3,21 @@ dotenv.config({
     path: "./.env"
 });
 import app from "./app.js"
+import connectDB from "./db/index.js"
+import dns from "dns"
+
+dns.setServers(["8.8.8.8", "8.8.4.4"])
+
 const port = process.env.PORT
 
 
-app.listen(port, () =>{
-    console.log(`Example app is running on the port : http://localhost:${port}`);
+connectDB()
+.then(() => {
+    app.listen(port, () => {
+        console.log(`Example app is listening on port http://localhost:${port}`);
+    })
+})
+.catch((err) => {
+    console.error("MongoDB connection error", err)
+    process.exit(1)
 })
